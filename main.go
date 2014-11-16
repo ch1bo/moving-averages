7f3efa65cab6299edd214711a5d9c5d67c789890
Sebastian Nagel <sebastian.nagel@ncoding.at>
2014-11-16 19:08:38 +0100

    added MarketDataPoint datastructure

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"image/color"
 	"log"
 	"net/http"
+	"os/exec"
 	"strconv"
 	"time"
 )
@@ -40,25 +41,58 @@ func fetchBitstampTicker() (ticker *TickerData, err error) {
 	return
 }
 
-func fetchBitstampMarketData() ([][]string, error) {
+type MarketDataPoint struct {
+	Time time.Time
+	BTC  float64
+	USD  float64
+}
+
+func fetchBitstampMarketData() ([]MarketDataPoint, error) {
 	res, err := http.Get("http://api.bitcoincharts.com/v1/trades.csv?symbol=bitstampUSD")
 	if err != nil {
 		return nil, err
 	}
 	defer res.Body.Close()
 	reader := csv.NewReader(res.Body)
-	return reader.ReadAll()
+	csvData, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+	data := make([]MarketDataPoint, len(csvData))
+	for i, row := range csvData {
+		t, err := strconv.ParseInt(row[0], 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		data[i].Time = time.Unix(t, 0)
+		data[i].BTC, _ = strconv.ParseFloat(row[1], 64)
+		data[i].USD, _ = strconv.ParseFloat(row[2], 64)
+	}
+	return data, nil
 }
 
-func plotMarketData(data [][]string) {
+func plotMarketData(data []MarketDataPoint, filename string) {
 	p, err := plot.New()
 	if err != nil {
 		panic(err)
 	}
+	p.X.Tick.Marker = func(min, max float64) []plot.Tick {
+		fmt.Printf("should create tick for min=%v max=%v\n", min, max)
+		return []plot.Tick{
+			plot.Tick{
+				Value: min,
+				Label: time.Unix(int64(min), 0).Format(time.Stamp),
+			},
+			plot.Tick{
+				Value: max,
+				Label: time.Unix(int64(max), 0).Format(time.Stamp),
+			},
+		}
+	}
 	pts := make(plotter.XYs, len(data))
 	for i := len(data) - 1; i >= 0; i-- {
-		pts[i].X, _ = strconv.ParseFloat(data[i][0], 64)
-		pts[i].Y, _ = strconv.ParseFloat(data[i][1], 64)
+		pts[i].X = float64(data[i].Time.Unix())
+		pts[i].Y = data[i].BTC
 	}
 	l, err := plotter.NewLine(pts)
 	if err != nil {
@@ -67,8 +101,9 @@ func plotMarketData(data [][]string) {
 	l.LineStyle.Color = color.RGBA{A: 255}
 	p.Add(l)
 	p.Legend.Add("price", l)
+	p.Add(plotter.NewGrid())
 	// Save the plot to a PNG file.
-	if err := p.Save(4, 4, "points.png"); err != nil {
+	if err := p.Save(14, 10, filename); err != nil {
 		panic(err)
 	}
 }
@@ -82,16 +117,16 @@ func main() {
 	}
 	fmt.Printf("Current bitstamp ticker data: %+v\n", *ticker)
 
-	var data [][]string
-	data, err = fetchBitstampMarketData()
+	data, err := fetchBitstampMarketData()
 	if err != nil {
 		log.Fatal("Error on fetching bitstamp market data: " + err.Error())
 	}
-	plotMarketData(data)
-	for i := len(data) - 1; i >= 0; i-- {
-		row := data[i]
-		unixtime, _ := strconv.ParseInt(row[0], 10, 64)
-		t := time.Unix(unixtime, 0)
-		fmt.Printf("%d: %s - %s %s\n", i, t, row[1], row[2])
-	}
+	plotMarketData(data, "points.svg")
+	// for i := len(data) - 1; i >= 0; i-- {
+	// 	row := data[i]
+	// 	unixtime, _ := strconv.ParseInt(row[0], 10, 64)
+	// 	t := time.Unix(unixtime, 0)
+	// 	fmt.Printf("%d: %s - %s %s\n", i, t, row[1], row[2])
+	// }
+	exec.Command("eog", "points.svg").Start()
 }
